5632228fca44f13ba9aaf8a3f24c4ca9cbeaa62e
Kari Fuller <38434486+karicfuller@users.noreply.github.com>
2023-12-20 06:32:11 -0800

    Added Basic_HTML_1/JavaScript files

---
+++ HTML and CSS Projects/Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,8 @@
+alert("Hello World! This is an alert box!");
+
+function My_First_Function(){
+    var String = "Kiss me, I'm Irish!"
+    var result = String.fontcolor("green");
+    document.getElementById("Irish").innerHTML =
+    result;
+}
